d7a489e1a933c59ae97dcc1978bff2f8cc848912
Ana Melem <anamelem4@gmail.com>
2022-05-14 14:24:24 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,18 @@
+import { ReactComponent as StarFull } from 'assets/img/full.svg';
+import { ReactComponent as StarHalf } from 'assets/img/half.svg';
+import { ReactComponent as StarEmpty } from 'assets/img/empty.svg';
+import './styles.css'
+
+function MovieStars() {
+  return (
+    <div className="dsmovie-stars-container">
+      <StarFull />
+      <StarFull />
+      <StarFull />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  );
+}
+
+export default MovieStars;
