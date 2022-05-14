9a788f4d99532d0354297ebf95ffdad7216deaa3
Ana Melem <anamelem4@gmail.com>
2022-05-14 12:21:50 -0300

    Navbar Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,7 @@
-import { ReactComponent as GithubIcon } from './assets/img/GithubIcon.svg'
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <header>
-      <nav className="container">
-        <div className="dsmovie-nav-content">
-          <h1>DSMovie</h1>
-          <a href="http://github.com/anamelem" target="_blank" rel="noreferrer">
-            <div className="dsmovie-contact-container">
-              <GithubIcon />
-              <p className="dsmovie-contact-link">/anamelem</p>
-            </div>
-          </a>
-        </div>
-      </nav>
-    </header>
+    <Navbar></Navbar>
   );
 }
 
